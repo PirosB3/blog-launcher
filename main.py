d28e57b8ee9aef860a53fdac60246ea84bc9c953
Daniel Pyrathon <pirosb3@gmail.com>
2015-04-04 15:53:03 +0200

    refactored

--- main.py
+++ main.py
@@ -43,6 +43,26 @@ def fetch_amazon_credentials(f):
 
     return wrapper
 
+def fetch_connection_and_instances(f):
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        amazon_id = kwargs['amazon_id']
+        amazon_secret = kwargs['amazon_secret']
+
+        # Get Amazon instance
+        kwargs['connection'] = make_instance(amazon_id, amazon_secret)
+
+        # Fetch tagged instances map
+        try:
+            kwargs['instances'] = get_tagged_instances(kwargs['connection'])
+        except boto.exception.EC2ResponseError, e:
+            abort(e.status)
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
 def get_tagged_instances(connection):
     all_reservations = connection.get_all_instances(
         filters={
@@ -65,42 +85,25 @@ def make_instance(amazon_id, amazon_secret):
 
 @app.route('/api/instances/<instance_id>', methods=['DELETE'])
 @fetch_amazon_credentials
-def stop_instance(instance_id, amazon_id, amazon_secret):
+@fetch_connection_and_instances
+def stop_instance(instance_id, connection, instances, amazon_id, amazon_secret):
 
-    # Get Amazon instance
-    conn = make_instance(amazon_id, amazon_secret)
-
-    # Fetch tagged instances map
     try:
-        tagged_instances = get_tagged_instances(conn)
-        try:
-            selected_instance = tagged_instances[instance_id]
-        except KeyError:
-            abort(404)
+        selected_instance = instances[instance_id]
+    except KeyError:
+        abort(404)
 
-        # Terminate instance
-        selected_instance.terminate()
-
-        abort(202)
-    except boto.exception.EC2ResponseError, e:
-        abort(e.status)
+    # Terminate instance
+    selected_instance.terminate()
+    return "", 202
 
 
 @app.route('/api/instances')
 @fetch_amazon_credentials
-def instances(amazon_id, amazon_secret):
-
-    # Get Amazon instance
-    conn = make_instance(amazon_id, amazon_secret)
-
-    # Fetch tagged instances map
-    try:
-        tagged_instances = get_tagged_instances(conn)
-
-        # Map each instance object to it's state
-        return flask.jsonify(tagged_instances)
-    except boto.exception.EC2ResponseError, e:
-        abort(e.status)
+@fetch_connection_and_instances
+def instances(connection, instances, amazon_id, amazon_secret):
+    # Map each instance object to it's state
+    return flask.jsonify(instances)
 
 if __name__ == '__main__':
     app.debug = True
