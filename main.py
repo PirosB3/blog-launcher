1542b755cd602635db0f937dd9cce96683dcca6f
Daniel Pyrathon <pirosb3@gmail.com>
2015-04-04 15:39:25 +0200

    added stop instance

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import abort
+from flask.json import JSONEncoder
 
 import boto
 import boto.ec2
@@ -13,6 +14,15 @@ app = Flask(__name__)
 AMAZON_REGION = "us-west-1"
 AMAZON_TAG_KEY = "provider"
 AMAZON_TAG_VALUE = "daniel-pyrathon"
+
+
+class AWSJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, boto.ec2.instance.Instance):
+            return obj.state.capitalize()
+        return JSONEncoder.default(self, obj)
+
 
 @app.route('/')
 def index():
@@ -53,6 +63,29 @@ def make_instance(amazon_id, amazon_secret):
         aws_secret_access_key=amazon_secret,
     )
 
+@app.route('/api/instances/<instance_id>', methods=['DELETE'])
+@fetch_amazon_credentials
+def stop_instance(instance_id, amazon_id, amazon_secret):
+
+    # Get Amazon instance
+    conn = make_instance(amazon_id, amazon_secret)
+
+    # Fetch tagged instances map
+    try:
+        tagged_instances = get_tagged_instances(conn)
+        try:
+            selected_instance = tagged_instances[instance_id]
+        except KeyError:
+            abort(404)
+
+        # Terminate instance
+        selected_instance.terminate()
+
+        abort(202)
+    except boto.exception.EC2ResponseError, e:
+        abort(e.status)
+
+
 @app.route('/api/instances')
 @fetch_amazon_credentials
 def instances(amazon_id, amazon_secret):
@@ -63,10 +96,13 @@ def instances(amazon_id, amazon_secret):
     # Fetch tagged instances map
     try:
         tagged_instances = get_tagged_instances(conn)
-        return flask.jsonify(items=sorted(tagged_instances.keys()))
+
+        # Map each instance object to it's state
+        return flask.jsonify(tagged_instances)
     except boto.exception.EC2ResponseError, e:
         abort(e.status)
 
 if __name__ == '__main__':
     app.debug = True
+    app.json_encoder = AWSJSONEncoder
     app.run()
